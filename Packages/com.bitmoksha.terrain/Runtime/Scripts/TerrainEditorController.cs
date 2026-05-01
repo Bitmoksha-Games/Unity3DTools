e640027f496f3c0ca5ef46c1e887b3fea5c37de0
parths <parths@rediffmail.com>
2026-05-01 21:33:48 +0530

    Fixed build compile error

--- Packages/com.bitmoksha.terrain/Runtime/Scripts/TerrainEditorController.cs
+++ Packages/com.bitmoksha.terrain/Runtime/Scripts/TerrainEditorController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
-using UnityEditorInternal.VR;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
